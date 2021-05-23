2d73898068473408f0997f02e1b5f825679ebf16
SITIC Developer <testsiticsas@gmail.com>
2021-05-23 12:16:45 -0500

    ViewModel Provider

---
+++ provider-back/Models/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace provider_back.Models
+{
+    public class ProviderViewModel
+    {
+        public int ProviderID { get; set; }
+        public int ProviderOrder { get; set; }
+        public string ProviderName { get; set; }
+        public string ProviderBusiness_Name { get; set; }
+        public string ProviderNIT { get; set; }
+        public string ProviderAddress { get; set; }
+        public string ProviderPhoneNumber { get; set; }
+        public DateTime ProviderCreation_Date { get; set; }
+        public DateTime? ProviderModification_Date { get; set; }
+        public decimal ProviderRating_Number { get; set; }
+    }
+}
