4f29e6124408549f291952fb6e79b362d0a9d426
SITIC Developer <testsiticsas@gmail.com>
2021-05-23 12:28:19 -0500

    Adding enumeration of actions in Utilities

---
+++ provider-back/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace provider_back
+{
+    public static class Utilities
+    {
+        public enum EnumAction: int
+        {
+            Insert,
+            Update,
+            Delete
+        }
+    }
+}
